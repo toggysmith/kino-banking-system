e6096d7ab43d4212b608dac1efb6cb049d5a07c1
Toggy Smith <toggysmith@gmail.com>
2023-04-25 20:14:58 +0100

    feat: add admin menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include "terminal/user_interface.hpp"
 
+using Terminal::UserInterface;
+
 enum class Service
 {
   customer_portal,
@@ -10,17 +12,63 @@ enum class Service
 Service
 ask_user_to_select_service()
 {
-  using Terminal::UserInterface;
+  UserInterface::show_heading("Services");
 
-  const std::string_view& selection =
-    UserInterface::show_options({ "Customer portal", "ATM", "Admin portal" });
+  std::vector<const std::string_view> options = { "Customer portal",
+                                                  "ATM",
+                                                  "Admin portal" };
 
-  if (selection == "Customer portal") {
+  const std::string_view& selection{ UserInterface::show_options(options) };
+
+  if (selection == options[0]) {
     return Service::customer_portal;
-  } else if (selection == "ATM") {
+  } else if (selection == options[1]) {
     return Service::atm;
-  } else if (selection == "Admin portal") {
+  } else if (selection == options[2]) {
     return Service::admin_portal;
+  }
+}
+
+void
+show_create_customer_menu()
+{
+}
+
+void
+show_view_customers_menu()
+{
+}
+
+void
+show_update_customer_records_menu()
+{
+}
+
+void
+show_delete_customer_menu()
+{
+}
+
+void
+run_admin_portal_service()
+{
+  UserInterface::show_heading("Admin Portal");
+
+  std::vector<const std::string_view> options = { "Create customer",
+                                                  "View customers",
+                                                  "Update customer records",
+                                                  "Delete customer" };
+
+  const std::string_view& selection{ UserInterface::show_options(options) };
+
+  if (selection == options[0]) {
+    show_create_customer_menu();
+  } else if (selection == options[1]) {
+    show_view_customers_menu();
+  } else if (selection == options[2]) {
+    show_update_customer_records_menu();
+  } else if (selection == options[3]) {
+    show_delete_customer_menu();
   }
 }
 
@@ -28,6 +76,12 @@ int
 main()
 {
   Service selected_service{ ask_user_to_select_service() };
+
+  switch (selected_service) {
+    case Service::admin_portal:
+      run_admin_portal_service();
+      break;
+  }
 
   return 0;
 }
