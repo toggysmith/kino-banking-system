04da87dbfce92706ce6113d0c18093298fabc1fd
Toggy Smith <toggysmith@gmail.com>
2023-04-25 21:10:30 +0100

    feat: add admin menu for adding new customers

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
+#include "core/customer.hpp"
 #include "terminal/user_interface.hpp"
+#include "util/date.hpp"
 
+using Core::Customer;
 using Terminal::UserInterface;
+using Util::Date;
+
+std::vector<Customer> customers;
 
 enum class Service
 {
@@ -32,10 +38,45 @@ ask_user_to_select_service()
 void
 show_create_customer_menu()
 {
-}
+  UserInterface::show_heading("Admin Portal / Create Customer");
 
-#include "core/customer.hpp"
-#include "util/date.hpp"
+  std::string forename;
+  std::string middle_names;
+  std::string last_name;
+  int date_of_birth_day;
+  int date_of_birth_month;
+  int date_of_birth_year;
+
+  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+  std::cout << "Enter the forename of the customer: ";
+  std::getline(std::cin, forename);
+  std::cout << "Enter the middle name(s) of the customer: ";
+  std::getline(std::cin, middle_names);
+  std::cout << "Enter the last name of the customer: ";
+  std::getline(std::cin, last_name);
+  std::cout << "Enter the day the customer was born: ";
+  std::cin >> date_of_birth_day;
+  std::cout << "Enter the month the customer was born: ";
+  std::cin >> date_of_birth_month;
+  std::cout << "Enter the year the customer was born: ";
+  std::cin >> date_of_birth_year;
+
+  const Name name{ forename, middle_names, last_name };
+
+  const Date date_of_birth{ date_of_birth_day,
+                            date_of_birth_month,
+                            date_of_birth_year };
+
+  const Customer customer{ name, date_of_birth, 1 };
+
+  customers.push_back(customer);
+
+  std::cout
+    << "\nCustomer created successfully. Press any character to proceed.";
+  std::cin.ignore();
+  std::cin.get();
+  std::cout << '\n';
+}
 
 void
 show_view_customers_menu()
@@ -55,23 +96,30 @@ show_delete_customer_menu()
 void
 run_admin_portal_service()
 {
-  UserInterface::show_heading("Admin Portal");
+  bool finished{ false };
 
-  std::vector<const std::string_view> options = { "Create customer",
-                                                  "View customers",
-                                                  "Update customer records",
-                                                  "Delete customer" };
+  while (!finished) {
+    UserInterface::show_heading("Admin Portal");
 
-  const std::string_view& selection{ UserInterface::show_options(options) };
+    std::vector<const std::string_view> options = { "Create customer",
+                                                    "View customers",
+                                                    "Update customer records",
+                                                    "Delete customer",
+                                                    "Exit" };
 
-  if (selection == options[0]) {
-    show_create_customer_menu();
-  } else if (selection == options[1]) {
-    show_view_customers_menu();
-  } else if (selection == options[2]) {
-    show_update_customer_records_menu();
-  } else if (selection == options[3]) {
-    show_delete_customer_menu();
+    const std::string_view& selection{ UserInterface::show_options(options) };
+
+    if (selection == options[0]) {
+      show_create_customer_menu();
+    } else if (selection == options[1]) {
+      show_view_customers_menu();
+    } else if (selection == options[2]) {
+      show_update_customer_records_menu();
+    } else if (selection == options[3]) {
+      show_delete_customer_menu();
+    } else if (selection == options[4]) {
+      finished = true;
+    }
   }
 }
 
