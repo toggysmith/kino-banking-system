de08f9b0fc22811be20e6e47e67577ca3690fc84
Toggy Smith <toggysmith@gmail.com>
2023-05-13 22:18:02 +0100

    feat: add error messages and prevent user from entering blank details

--- src/menus/create_new_manager_menu.cpp
+++ src/menus/create_new_manager_menu.cpp
@@ -7,7 +7,6 @@
 
 #include "../core/database_manager.hpp"
 
-#include <iostream>
 namespace Menus {
 
 CreateNewManagerMenu::CreateNewManagerMenu()
@@ -39,11 +38,46 @@ CreateNewManagerMenu::render(
   Core::DatabaseManager* database_manager =
     Core::DatabaseManager::get_instance();
 
-  if (ImGui::Button("Submit")) {
-    std::string sql = "INSERT INTO manager VALUES('" + std::string(name) +
-                      "', '" + std::string(password) + "');";
+  std::vector<const char*> error_messages;
 
-    database_manager->run_sql(sql.c_str());
+  bool does_name_exist = !std::string(name).empty();
+  bool does_password_exist = !std::string(password).empty();
+  bool does_confirm_password_exist = !std::string(confirm_password).empty();
+  bool does_confirm_password_match_password =
+    std::string(password) == std::string(confirm_password);
+
+  if (!does_name_exist) {
+    error_messages.push_back("Name must be specified.");
+  }
+
+  if (!does_password_exist) {
+    error_messages.push_back("Password must be specified.");
+  }
+
+  if (!does_confirm_password_exist) {
+    error_messages.push_back("Confirm password must be specified.");
+  }
+
+  if (!does_confirm_password_match_password && does_password_exist &&
+      does_confirm_password_exist) {
+    error_messages.push_back("Passwords don't match.");
+  }
+
+  ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1.0, 0.0, 0.0, 1.0));
+  for (const auto& error_message : error_messages) {
+    ImGui::BulletText("%s", error_message);
+  }
+  ImGui::PopStyleColor();
+
+  if (error_messages.empty()) {
+    if (ImGui::Button("Create")) {
+      std::string sql = "INSERT INTO manager VALUES('" + std::string(name) +
+                        "', '" + std::string(password) + "');";
+
+      database_manager->run_sql(sql.c_str());
+
+      menu_stack.pop_front();
+    }
   }
 }
 
